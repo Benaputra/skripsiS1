b58abc96eaf7f0d1f12da1b2efed510ac4b11050
Roudhi Benaputra <roudhi.benaputra@gmail.com>
2023-10-30 20:49:34 +0700

    register part II

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -29,11 +29,11 @@ License: You must have a valid license purchased only from themeforest(the above
                 <!-- BEGIN: Register Info -->
                 <div class="hidden xl:flex flex-col min-h-screen">
                     <a href="" class="-intro-x flex items-center pt-5">
-                        <img alt="Midone - HTML Admin Template" class="w-6" src="dist/images/logo.svg">
+                        <img alt="Midone - HTML Admin Template" class="w-6" src="template/dist/images/logo.svg">
                         <span class="text-white text-lg ml-3"> FASTA UPB </span> 
                     </a>
                     <div class="my-auto">
-                        <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="dist/images/illustration.svg">
+                        <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="template/dist/images/illustration.svg">
                         <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">
                             A few more clicks to 
                             <br>
